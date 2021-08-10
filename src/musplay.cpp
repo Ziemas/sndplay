bfbb8c59c4b79dc0b85f9b18c5a49543ce6a6111
Ziemas <ziemas@ziemas.se>
2021-08-10 06:04:58 +0200

    A bit more loading

--- src/musplay.cpp
+++ src/musplay.cpp
@@ -1,14 +1,31 @@
 #include "musplay.h"
 #include <cstdio>
-#include <fstream>
+#include <iterator>
 #include <memory>
 
-std::fstream file;
+std::vector<SoundBank> gBanks;
 
 int loadSongs() { return 0; }
 
-int loadBank(char *filename) {
+void SoundBank::Load(sbv2Struct *bank) {
+    sbParams = *bank;
 
+    auto instr = (Instrument *)((u8 *)bank + bank->instrumentOffset);
+
+    for (int i = 0; i < bank->instrumentCount; i++) {
+        auto region = (Region *)((u8 *)bank + instr->oRegion);
+
+        std::vector<Region> rvec;
+        for (int j = 0; i < instr->nRegion; i++)
+            rvec.push_back(region[j]);
+
+        regions.emplace(i, rvec);
+        instruments.push_back(instr[i]);
+    }
+}
+
+int loadBank(char *filename) {
+    std::fstream file;
     file.open(filename, std::fstream::binary | std::fstream::in);
     file.seekg(0, std::fstream::beg);
 
@@ -32,7 +49,7 @@ int loadBank(char *filename) {
         size += 4;
     }
 
-    auto *soundBank = (SoundBank *)malloc(size);
+    auto *soundBank = (sbv2Struct *)malloc(size);
     if (soundBank == nullptr) {
         printf("malloc failed\n");
         return -1;
@@ -50,6 +67,10 @@ int loadBank(char *filename) {
     file.seekg(header.bankOffset, std::fstream::beg);
     file.read((char *)soundBank, header.sbSize);
 
+    SoundBank sb;
+    sb.Load(soundBank);
+    gBanks.push_back(sb);
+
     if (header.sampleSize == 0) {
         printf("%s: bank size %d\n", filename, header.sampleSize);
         printf("unhandled!\n");
@@ -64,6 +85,8 @@ int loadBank(char *filename) {
 
     file.seekg(header.sampleOffset, std::fstream::beg);
     file.read((char *)sampleBuf, header.sampleSize);
+    for (u32 i = 0; i < header.sampleSize; i++)
+        sb.samplebuf.push_back(sampleBuf[i]);
 
     free(sampleBuf);
     free(soundBank);
@@ -74,5 +97,21 @@ int loadBank(char *filename) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc > 1)
+        loadBank(argv[1]);
+
+    for (auto b : gBanks) {
+        printf("fourcc: %.*s\n", 4, (char *)&b.sbParams.fourcc);
+        printf("bank: %.*s\n", 4, (char *)&b.sbParams.name);
+        //printf("instruments:\n");
+        //for (auto i : b.instruments) {
+        //    printf("regions: %x\n", i.nRegion);
+        //    printf("vol?: %x\n", i.volume);
+        //    printf("unk: %x\n", i.something);
+        //    printf("regionidx: %x\n", i.oRegion);
+        //    printf("----:\n");
+        //}
+    }
+
     return 0;
 }
--- src/musplay.h
+++ src/musplay.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "types.h"
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <vector>
 
 // Big thanks to rerwarwar for struct definitions
+
+#define FOURCC(a, b, c, d)                                                     \
+    ((uint32)(((d) << 24) | ((c) << 16) | ((b) << 8) | (a)))
 
 enum class BankFlags : u32 {
     SB_PTRS_REWRITTEN = 0x1,
@@ -14,6 +18,20 @@ enum class BankFlags : u32 {
 enum class FileType : u32 {
     SBv2 = 1,
     SBlk = 3,
+};
+
+class BinReader {
+  public:
+    BinReader(std::fstream &buf) : m_buf(buf){};
+
+    template <typename T> void Read(T *ptr) {
+        m_buf.read((char *)ptr, sizeof(T));
+    }
+
+    void seek(u32 pos) { m_buf.seekg(pos, std::fstream::beg); }
+
+  private:
+    std::fstream &m_buf;
 };
 
 // SBlk files have their sbHeader at 0x800 into the file
@@ -30,15 +48,15 @@ struct sbHeader {
 } __attribute__((packed));
 
 struct sbv2Struct {
-    /* 0x0  */ u32 signature; // "SBv2"
-    /* 0x4  */ u32 version;   // maybe
+    /* 0x0  */ u32 fourcc;  // "SBv2"
+    /* 0x4  */ u32 version; // maybe
     /* 0x8  */ u32 unkFlags;
-    /* 0xC  */ u32 name;      // eg "V1RA" or "BOSS"
-    /* 0X10 */ u32 unk;       // 0xa some kind of identifier?
-    /* 0x14 */ u16 songCount; // always 1 for jak banks?
-    /* 0x16 */ u16 nMap;      // ?
-    /* 0x18 */ u16 instrumentCount;
-    /* 0x1A */ u16 regionCount;
+    /* 0xC  */ u32 name;            // eg "V1RA" or "BOSS"
+    /* 0X10 */ u32 unk;             // 0xa some kind of identifier?
+    /* 0x14 */ u16 songCount;       // always 1 for jak banks?
+    /* 0x16 */ u16 instrumentCount; // ?
+    /* 0x18 */ u16 regionCount;
+    /* 0x1A */ u16 sampleCount;
     /* 0x1C */ u32 songOffset;       // from start of struct
     /* 0x20 */ u32 instrumentOffset; // these are all rewritten into ptrs
     /* 0x24 */ u32 regionOffset;     //
@@ -84,14 +102,13 @@ struct Region { // 0x18 B
     u32 sampleID; // I think it's the index in the sample bank...
 } __attribute__((packed));
 
-class SoundBank {
-  public:
-    sbv2Struct sb;
-    std::map<u32, Region> regions;
+struct SoundBank {
+    sbv2Struct sbParams;
     std::vector<Instrument> instruments;
+    std::map<u32, std::vector<Region>> regions;
     std::vector<u8> samplebuf;
 
-  private:
+    void Load(sbv2Struct *bank);
 };
 
 struct seq {
@@ -102,16 +119,16 @@ struct seq {
 } __attribute__((packed));
 
 struct MMID {
-    char signature[4]; // "MMID"
-    u16 type;          //?
-    u8 a;              //?
-    u8 nTrack;         // maybe... it's always 3 lol
-    char name[4];      // e.g. "V1RA"
-    u32 blank;         //? next in list? followed by ptrs to tracks
+    u32 fourcc;   // MMID
+    u16 type;     //?
+    u8 a;         //?
+    u8 nTrack;    // maybe... it's always 3 lol
+    char name[4]; // e.g. "V1RA"
+    u32 blank;    //? next in list? followed by ptrs to tracks
 } __attribute__((packed));
 
 struct MID {
-    char signature[4]; // "MID "
+    u32 fourcc; // MID
     u16 a;
     u16 b;
     u32 c;
